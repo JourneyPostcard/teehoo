bbadc3c0ca9d0079e18feedaae89fc690d5d814e
JerryTsui <jerrytsui91@gmail.com>
2021-05-09 19:02:44 +0800

    feat:首页移动端导航栏样式完成

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class='navBar'>
-    <img src='@/assets/logo.png' alt=''>
-    <router-link to='/'>首页</router-link>
-    <router-link to='/'>品牌故事</router-link>
-    <router-link to='/'>在线够买</router-link>
-    <router-link to='/'>联系方式</router-link>
+  <div class='navBarContainer'>
+    <div class='navBar'>
+      <img src='@/assets/logo.png' alt=''>
+      <router-link to='/'>首页</router-link>
+      <router-link to='/'>品牌故事</router-link>
+      <router-link to='/'>在线够买</router-link>
+      <router-link to='/'>联系方式</router-link>
+    </div>
+    <div class='navBarMobile' :class='{spread:isSpread}'>
+      <div class='logoContainer'>
+        <img src='@/assets/logo.png' alt='' @click='toggleSpread'>
+      </div>
+      <div class='linkContainer'>
+        <div class='linkBg' @click='toggleSpread'></div>
+        <div class='linkContent'>
+          <router-link to='/'>首页</router-link>
+          <router-link to='/'>品牌故事</router-link>
+          <router-link to='/'>在线够买</router-link>
+          <router-link to='/'>联系方式</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +28,18 @@
 import './NavBar.scss'
 import './NavBar2.scss'
 
-export default {}
+export default {
+  data() {
+    return {
+      isSpread: false,
+    }
+  },
+  methods: {
+    toggleSpread() {
+      this.isSpread = !this.isSpread
+    },
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
